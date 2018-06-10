7abf2d95ac061943b31412d7406b8a2bf580cc78
Antonio Cortes <antoni_anemi@hotmail.com>
2018-06-10 14:40:29 -0700

    autentication working

--- barbershop/barbershop/ViewController.swift
+++ barbershop/barbershop/ViewController.swift
@@ -7,14 +7,28 @@
 //
 
 import UIKit
-
+import Firebase
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txt_username: UITextField!
+    @IBOutlet weak var txt_password: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-
+    @IBAction func logIn(_ sender: Any) {
+        guard let username = txt_username.text, let password = txt_password.text else{ return }
+        Auth.auth().signIn(withEmail: username, password: password) { (authResult, error) in
+            if error != nil{
+                return
+            }
+            let res = authResult
+        }
+    }
+    
 }
 
